a46e83360023d9d0daf2a10a67b9f70138e0b565
MyPro <brookbirch@gmail.com>
2018-06-25 14:11:36 -0500

    add front-end of blog section

--- client/src/components/footerComponent/footer.js
+++ client/src/components/footerComponent/footer.js
@@ -11,10 +11,10 @@ class Footer extends Component {
          <footer>
          <nav>
             <ul>
-                <li><a href="#" class="fa fa-facebook"></a></li>
-                <li><a href="#" class="fa fa-twitter"></a></li>
-                <li><a href="#" class="fa fa-github"></a></li>
-                <li><a href="#" class="fa fa-instagram"></a></li>
+                <li><a href="http://www.facebook.com" target ="_blank" class="fa fa-facebook"></a></li>
+                <li><a href="http://www.twitter.com" target ="_blank" class="fa fa-twitter"></a></li>
+                <li><a href="http://www.github.com" target ="_blank" class="fa fa-github"></a></li>
+                <li><a href="http://www.instagram.com" target ="_blank" class="fa fa-instagram"></a></li>
             </ul>
         </nav>  
         </footer>
